20c1f85c7731a9a65bdb88e69bed4f41ec26c492
SemenovykhRA <evelenc.live@gmail.com>
2025-05-27 21:56:40 +0300

    Диплом. React

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header';
+import ArtistsPage from './pages/ArtistsPage';
+import TracksPage from './pages/TracksPage';
+import SearchPage from './pages/SearchPage';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+/**
+ * Основной компонент приложения с маршрутизацией.
+ */
+const App: React.FC = () => (
+  <Router>
+    <Header />
+    <main>
+      <Routes>
+        <Route path="/" element={<Navigate to="/search" replace />} />
+        <Route path="/artists" element={<ArtistsPage />} />
+        <Route path="/tracks" element={<TracksPage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
+    </main>
+  </Router>
+);
 
-// export default App;
+export default App;
